160d0181a9c9891b359db247f715304b9782b209
CMRHDL <cmrhdl1337@gmail.com>
2021-02-10 13:48:27 +0100

    Play more loosely

--- Player.js
+++ Player.js
@@ -1,12 +1,19 @@
 const utils = require("./utils");
 
 const {
-  highCards,
-  hasPocketPair,
+  hasAce,
+  hasAtLeast,
   hasAtLeastOneHighCard,
-  isSuited,
   hasHitPair,
+  hasPocketPair,
+  highCards,
+  isSuited,
 } = utils;
+
+const early = 100;
+const mid = 50;
+const late = 20;
+const end = 10;
 
 class Player {
   static get VERSION() {
@@ -14,14 +21,44 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    const [firstCard, secondCard] = gameState.players[
-      gameState.in_action
-    ].hole_cards;
+    const player = gameState.players[gameState.in_action];
+    const [firstCard, secondCard] = player.hole_cards;
 
     const strongStartingHand =
       highCards.includes(firstCard.rank) && highCards.includes(secondCard.rank);
 
     if (strongStartingHand) {
+      return bet(10000);
+    }
+
+    if (player.stack / gameState.small_blind < early) {
+      // pretty good hand
+      if (hasPocketPair(firstCard, secondCard) && hasAtLeast(firstCard, secondCard, "10")) {
+        return bet(10000);
+      }
+    }
+
+    if (player.stack / gameState.small_blind < mid) {
+      // decent hand
+      if (hasAce(firstCard, secondCard) && isSuited(firstCard, secondCard)) {
+        return bet(10000);
+      }
+
+      if (hasPocketPair(firstCard, secondCard) && hasAtLeast(firstCard, secondCard, "8")) {
+        return bet(10000);
+      }
+    }
+
+    if (player.stack / gameState.small_blind < late) {
+      // okish hand
+      if (hasPocketPair(firstCard, secondCard) && hasAtLeast(firstCard, secondCard, "5")) {
+        return bet(10000);
+      }
+      return bet(10000);
+    }
+
+    if (player.stack / gameState.small_blind < end) {
+      // nothing hand
       return bet(10000);
     }
 
--- utils/index.js
+++ utils/index.js
@@ -6,20 +6,48 @@ const highCards = [
   // "10",
 ];
 
+const cards = [
+  "A",
+  "K",
+  "Q",
+  "J",
+  "10",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+]
+
+const toRanks = (card) => card.rank;
+const containsCard = rank => (card) => card.rank === rank;
+
 const utils = {
   highCards,
   hasPocketPair: (firstCard, secondCard) => {
     return firstCard.rank === secondCard.rank;
+  },
+  hasHighCards: (firstCard, secondCard) => {
+    return firstCard.rank === "A" || secondCard.rank === "A";
+  },
+  hasAce: (firstCard, secondCard) => {
+    return firstCard.rank === "A" || secondCard.rank === "A";
+  },
+  hasAtLeast: (firstCard, secondCard, card) => {
+    const cardIndex = cards.indexOf(card);
+    return cards.indexOf(firstCard.rank) <= cardIndex || cards.indexOf(secondCard.rank) <= cardIndex;
+  },
+  isSuited: (firstCard, secondCard) => {
+    return firstCard.suit === secondCard.suit;
   },
   hasAtLeastOneHighCard: (firstCard, secondCard) => {
     return (
       highCards.includes(firstCard.rank) || highCards.includes(secondCard.rank)
     );
   },
-  isSuited: (firstCard, secondCard) => {
-    return firstCard.suit === secondCard.suit;
-  },
-  hasHitPair: (firstCard, secondCard, community_cards) => {},
 };
 
 module.exports = utils;
